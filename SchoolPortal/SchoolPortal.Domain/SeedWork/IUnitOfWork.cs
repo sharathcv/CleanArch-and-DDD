3e7754f7808d26b244a3591aff3333693d729151
sharathcv <sharathcv@gmail.com>
2025-05-07 16:57:12 +1000

    SchoolDbContext and UnitOfWork updated

--- SchoolPortal/SchoolPortal.Domain/SeedWork/IUnitOfWork.cs
+++ SchoolPortal/SchoolPortal.Domain/SeedWork/IUnitOfWork.cs
@@ -2,6 +2,6 @@
 {
     public interface IUnitOfWork: IDisposable
     {
-
+        Task<bool> SaveAsync(CancellationToken cancellationToken = default);
     }
 }
--- SchoolPortal/SchoolPortal.Infrastructure/SchoolContext.cs
+++ SchoolPortal/SchoolPortal.Infrastructure/SchoolContext.cs
@@ -1,9 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SchoolPortal.Domain.Entities;
+using SchoolPortal.Domain.Entities.StudentAggregate;
 using SchoolPortal.Domain.SeedWork;
+using System.Reflection;
 
 namespace SchoolPortal.Infrastructure
 {
     public class SchoolContext : DbContext, IUnitOfWork
     {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Course> Course { get; set; }
+        public DbSet<StudentCourse> StudentCourse { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Todo> Todo { get; set; }
+
+        public async Task<bool> SaveAsync(CancellationToken cancellationToken = default)
+        {
+            await base.SaveChangesAsync(cancellationToken);
+            return true;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }

0cded4ba4b8b7b994bf8103cbc9dc1721265a709
sharathcv <sharathcv@gmail.com>
2025-08-10 00:24:08 +0530

    Application update to add DbContext and formatting changes

--- SchoolPortal/SchoolPortal.Application/Program.cs
+++ SchoolPortal/SchoolPortal.Application/Program.cs
@@ -1,45 +1,47 @@
+using Microsoft.EntityFrameworkCore;
 using SchoolPortal.Application.CQRS.Queries;
 using SchoolPortal.Domain.SeedWork;
+using SchoolPortal.Infrastructure;
 using SchoolPortal.Infrastructure.Repository;
 using System.Reflection;
 
-namespace SchoolPortal.Application
+namespace SchoolPortal.Application;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        // Inject the generic repository as scoped
+        builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        builder.Services.AddScoped<DepartmentQuery>();
+        builder.Services.AddMediatR(m => m.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+        builder.Services.AddDbContext<SchoolContext>(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            options.UseNpgsql(builder.Configuration["ConnectionString"]);
+        });
 
-            // Add services to the container.
+        var app = builder.Build();
 
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-
-            // Inject the generic repository as scoped
-            builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-            builder.Services.AddScoped<DepartmentQuery>();
-            builder.Services.AddMediatR(m => m.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
-            // Add db context injection here
-            // 
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            // to get swagger back add below UseSwaggerUI() call
-            app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "School Portal Swagger"));
-
-            //app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
         }
+
+        // to get swagger back add below UseSwaggerUI() call
+        app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "School Portal Swagger"));
+
+        //app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }

37666bb49160c0d9ad44bdb45e1953135d4293a7
sharathcv <sharathcv@gmail.com>
2025-08-09 20:00:01 +0530

    Update with comment for future understanding in IAggregateRoot

--- SchoolPortal/SchoolPortal.Domain/SeedWork/IAggregateRoot.cs
+++ SchoolPortal/SchoolPortal.Domain/SeedWork/IAggregateRoot.cs
@@ -1,6 +1,6 @@
-﻿namespace SchoolPortal.Domain.SeedWork
+﻿namespace SchoolPortal.Domain.SeedWork;
+
+// This is just a marker interface
+public interface IAggregateRoot
 {
-    public interface IAggregateRoot
-    {
-    }
 }

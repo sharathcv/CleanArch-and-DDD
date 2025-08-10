31a4c83579890a322d347ce01c6d4e48bf32d215
sharathcv <sharathcv@gmail.com>
2025-08-10 08:37:59 +0530

    Updated DB Context

--- SchoolPortal/SchoolPortal.Infrastructure/SchoolContext.cs
+++ SchoolPortal/SchoolPortal.Infrastructure/SchoolContext.cs
@@ -1,35 +1,44 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using SchoolPortal.Domain.Entities;
 using SchoolPortal.Domain.Entities.StudentAggregate;
 using SchoolPortal.Domain.SeedWork;
 using System.Reflection;
 
-namespace SchoolPortal.Infrastructure
+namespace SchoolPortal.Infrastructure;
+
+public class SchoolContext : DbContext, IUnitOfWork
 {
-    public class SchoolContext : DbContext, IUnitOfWork
+    private readonly IMediator _mediator;
+
+    public SchoolContext(DbContextOptions<SchoolContext> options, IMediator mediator) : base(options)
     {
-        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
-        {
-        }
+        _mediator = mediator;
+    }
 
-        public DbSet<Student> Student { get; set; }
-        public DbSet<Course> Course { get; set; }
-        public DbSet<StudentCourse> StudentCourse { get; set; }
-        public DbSet<Department> Department { get; set; }
-        public DbSet<Todo> Todo { get; set; }
+    // DbSets for each entity in the domain model   
+    public DbSet<Student> Student { get; set; }
+    public DbSet<Course> Course { get; set; }
+    public DbSet<StudentCourse> StudentCourse { get; set; }
+    public DbSet<Department> Department { get; set; }
+    public DbSet<Todo> Todo { get; set; }
 
-        public async Task<bool> SaveAsync(CancellationToken cancellationToken = default)
-        {
-            await base.SaveChangesAsync(cancellationToken);
-            return true;
-        }
+    public async Task<bool> SaveAsync(CancellationToken cancellationToken = default)
+    {
+        await base.SaveChangesAsync(cancellationToken);
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // Separate configuration file will be there for each entity which we will pick up from assembly
-            // refer to folder "EntityConfigurations"
-            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-            base.OnModelCreating(modelBuilder);
-        }
+        // After a successful save, dispatch domain events
+        await _mediator.Publish(this);
+
+        return true;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Separate configuration file will be there for each entity which will be picked up from assembly
+        // (refer to folder "EntityConfigurations")
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
     }
 }

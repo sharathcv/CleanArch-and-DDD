93fd5ce686a584810086faaf30d4756dab0420d6
sharathcv <sharathcv@gmail.com>
2025-08-10 09:47:49 +0530

    Repository initial changes completed

---
+++ SchoolPortal/SchoolPortal.Domain/SeedWork/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace SchoolPortal.Domain.SeedWork;
+
+public interface IGenericRepository<T> where T: Entity, IAggregateRoot
+{
+    //IEnumerable<T> Specify(ISpecification<T> spec);
+    //IEnumerable<T> Specify2(ISpecification<T> spec);
+    IUnitOfWork UnitOfWork { get; }
+    Task<T> AddAsync(T entity);
+    Task DeleteAsync(T entity);
+    Task<T> GetByIdAsync(int Id);
+    Task<T?> GetAsync(Expression<Func<T,bool>> predicate);
+    //Task<T> GetAsync(ISpecification<T> spec);
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task UpdateAsync(T entity);
+}
---
+++ SchoolPortal/SchoolPortal.Infrastructure/Repository/GenericRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolPortal.Domain.SeedWork;
+using System.Linq.Expressions;
+
+namespace SchoolPortal.Infrastructure.Repository;
+
+public class GenericRepository<T>: IGenericRepository<T> where T: Entity, IAggregateRoot
+{
+    protected readonly SchoolContext _context;
+    internal DbSet<T> _set;
+
+    public IUnitOfWork UnitOfWork => _context;
+
+    public GenericRepository(SchoolContext context)
+    {
+        _context = context;
+        _set = _context.Set<T>();
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        await _set.AddAsync(entity);
+        return entity;
+    }
+
+    public Task DeleteAsync(T entity)
+    {
+        _set.Remove(entity);
+        return Task.CompletedTask;
+    }
+
+    public async Task<T?> GetAsync(Expression<Func<T, bool>> predicate)
+    {
+        var data = await _set.FirstOrDefaultAsync(predicate);
+        return data;
+    }
+
+    public async Task<IReadOnlyList<T>> GetAllAsync()
+    {
+        return await _set.ToListAsync();
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        var data = await _set.FindAsync(id);
+        if (data == null)
+        {
+            throw new KeyNotFoundException($"Entity of type {typeof(T).Name} with ID {id} not found.");
+        }
+
+        return data;
+    }
+
+    public Task UpdateAsync(T entity)
+    {
+        _set.Update(entity);
+        return Task.CompletedTask;
+    }
+}

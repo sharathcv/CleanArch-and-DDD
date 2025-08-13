181624c26338359c3b18ac2082648908bf0e5bb8
sharathcv <sharathcv@gmail.com>
2025-08-14 00:33:02 +0530

    CourseEnrolled event

---
+++ SchoolPortal/SchoolPortal.Domain/DomainEvents/CourseEnrolledEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace SchoolPortal.Domain.DomainEvents
+{
+    public class CourseEnrolledEvent 
+    {
+        public int StudentId { get; }
+        public int CourseId { get; }
+
+        public CourseEnrolledEvent(int studentId, int courseId)
+        {
+            StudentId = studentId;
+            CourseId = courseId;
+        }
+    }
+}
